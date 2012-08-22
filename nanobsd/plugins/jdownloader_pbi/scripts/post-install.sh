5d1fd23cfcf62b93279b3344dae1f1295db6e697
ProtoSD <sdproto@gmail.com>
2012-08-22 13:33:19 -0700

    Added and alphabetized dependencies in pbi.con

--- nanobsd/plugins/jdownloader_pbi/scripts/post-install.sh
+++ nanobsd/plugins/jdownloader_pbi/scripts/post-install.sh
@@ -5,6 +5,7 @@ echo libz.so.4 libz.so.5 > /usr/pbi/jdownloader-`uname -m`/etc/libmap.conf
 
 #cp -a /usr/local/sbin/jdownloader /usr/pbi/jdownloader-`uname -m`/sbin/jdownloader
 #chown www:www /usr/pbi/jdownloader-`uname -m`/sbin/jdownloader
+chown -R www:www /usr/pbi/jdownloader-`uname -m`/
 
 
 mkdir -p /usr/pbi/jdownloader-`uname -m`/etc/jdownloader/home
@@ -15,7 +16,8 @@ mkdir -p www:www /usr/pbi/jdownloader-`uname -m`/downloads
 chown www:www /usr/pbi/jdownloader-`uname -m`/downloads
 chmod 775 /usr/pbi/jdownloader-`uname -m`/downloads
 
-rm -rf /usr/pbi/jdownloader-`uname -m`/usr
+# Remove the port tree
+#rm -rf /usr/pbi/jdownloader-`uname -m`/usr
 #/usr/pbi/jdownloader-amd64/usr/ports/net/jdownloader
 
 echo $JAIL_IP"	"`hostname` >> /etc/hosts
