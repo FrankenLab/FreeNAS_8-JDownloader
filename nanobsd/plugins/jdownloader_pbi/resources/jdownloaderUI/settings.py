c959dcd005fbd7157ac6ab655f551439f35d0ddc
ProtoSD <sdproto@gmail.com>
2012-08-23 14:42:23 -0700

    Somehow default.png was deleted, renamed JDownloaderUI to jdownloaderUI

--- nanobsd/plugins/jdownloader_pbi/resources/jdownloaderUI/settings.py
+++ nanobsd/plugins/jdownloader_pbi/resources/jdownloaderUI/settings.py
@@ -85,7 +85,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '@g5*!x1%)nbl$^$HSH!((!_qgvub5a!jw!kj39--(xu$dx8iwg'
+SECRET_KEY = '@g5*!x1%)(T%WEJGL1jlghh81vub5a!jw!kj39--(xu$dx8iwg'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
