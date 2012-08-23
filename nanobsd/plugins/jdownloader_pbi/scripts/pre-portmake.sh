15add16c51d42aa927c5a6c212b9bbe0258deba9
ProtoSD <sdproto@gmail.com>
2012-08-22 17:00:06 -0700

    Fixed typo in pre-portmake, inserted "resources"

--- nanobsd/plugins/jdownloader_pbi/scripts/pre-portmake.sh
+++ nanobsd/plugins/jdownloader_pbi/scripts/pre-portmake.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cp -a /workspace2/8.2.0/nanobsd/plugins/jdownloader_pbi/usr/port/net/jdownloader /workspace2/8.2.0/FreeBSD/ports/net/
+cp -a /workspace2/8.2.0/nanobsd/plugins/jdownloader_pbi/resources/usr/port/net/jdownloader /workspace2/8.2.0/FreeBSD/ports/net/
