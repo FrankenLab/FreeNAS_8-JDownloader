3cda600eaed5696c5de4efa277da2abd8d87cb49
ProtoSD <sdproto@gmail.com>
2012-08-24 01:56:09 -0700

    Tag _14 works in the tree-menu, however Serviio disappeared from the menu, also submitted port.

--- nanobsd/plugins/jdownloader_pbi/resources/jdownloaderUI/freenas/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class JDownloader(models.Model):
-    """
-    Django model describing every tunable setting for jdownloader
-    """
-
-    enable = models.BooleanField(default=False)
--- nanobsd/plugins/jdownloader_pbi/resources/jdownloaderUI/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-urlpatterns = patterns('',
-     url(r'^plugins/jdownloader/', include('jdownloaderUI.freenas.urls')),
-)
