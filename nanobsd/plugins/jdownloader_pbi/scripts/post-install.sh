85e5065ab1f7bbad73c0cafa4bbeede9c249600f
ProtoSD <sdproto@gmail.com>
2012-09-08 19:38:57 -0700

    Too much trouble trying to use SED to edit wrapper script, created template to copy instead.

--- nanobsd/plugins/jdownloader_pbi/scripts/post-install.sh
+++ nanobsd/plugins/jdownloader_pbi/scripts/post-install.sh
@@ -53,6 +53,22 @@ sed -i '' -e "s,exec java,exec ${JDOWNLOADER_HOME}/bin/java,g" ${JDOWNLOADER_HOM
 #sed -i '' -e "12a\\
 #setenv FontPath \"/usr/local/lib/X11/fonts/\"" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
+# Need to add these lines after LOGFILE line
+sed -i '' -e "5a\\
+XVFB_ENABLE=`grep Xvfb /usr/pbi/jdownloader-amd64/etc/jdownloader.conf | sed -e \"s,Xvfb_Enable= ,,\"`" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+sed -i '' -e "6a\\
+echo \$XVFB_ENABLE" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+sed -i '' -e "7a\\
+XDISPLAY=`grep \"X11_Display= \" /usr/pbi/jdownloader-amd64/etc/jdownloader.conf | sed -e \"s,X11_Display= ,,\"`" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+#echo $XDISPLAY
+
+sed -i '' -e "8a\\
+DISPLAY=\${XDISPLAY}" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+#export DISPLAY
 
 # Need to test PIDfile because if user quits from X11 session FreeNAS GUI doesn't know
 # Test if PIDfile exists, add to sbin/jdownloader, need to see if FreeNAS GUI can be refreshed
@@ -60,76 +76,76 @@ sed -i '' -e "s,exec java,exec ${JDOWNLOADER_HOME}/bin/java,g" ${JDOWNLOADER_HOM
 #sed -i '' -e "4a\\
 #^M" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "5a\\
+sed -i '' -e "9a\\
 if [ -f /var/run/JDownloader/JDownloader.pid ]; then" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "6a\\
-\ \ \ \ if [ -z \{$id} ]; then" ${JDOWNLOADER_HOME}/sbin/jdownloader
-
-sed -i '' -e "7a\\
-\ \ \ \ \ \ \ \ exit" ${JDOWNLOADER_HOME}/sbin/jdownloader
-
-sed -i '' -e "8a\\
-\ \ \ \ fi" ${JDOWNLOADER_HOME}/sbin/jdownloader
-
-sed -i '' -e "9a\\ 
-\ \ \ \ id=\`cat /var/run/JDownloader/JDownloader.pid\`" ${JDOWNLOADER_HOME}/sbin/jdownloader   
-
 sed -i '' -e "10a\\
-\ \ \ \ echo \${id}" ${JDOWNLOADER_HOME}/sbin/jdownloader
+\ \ \ \ if [ -z {\$id} ]; then" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
 sed -i '' -e "11a\\
-\ \ \ \ if ps -p \${id} > /dev/null" ${JDOWNLOADER_HOME}/sbin/jdownloader                     
-
-sed -i '' -e "12a\\
-\ \ \ \ \ \ \ \ then" ${JDOWNLOADER_HOME}/sbin/jdownloader
-
-sed -i '' -e "13a\\
-\ \ \ \ \ \ \ \ echo \"Another copy of JDownloader appears to be running already."\" ${JDOWNLOADER_HOME}/sbin/jdownloader
-
-sed -i '' -e "14a\\
 \ \ \ \ \ \ \ \ exit" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "15a\\
-\ \ \ \ else" ${JDOWNLOADER_HOME}/sbin/jdownloader
-
-sed -i '' -e "16a\\
-\ \ \ \ \ \ \ \ rm /var/run/JDownloader/JDownloader.pid" ${JDOWNLOADER_HOME}/sbin/jdownloader
-
-sed -i '' -e "17a\\
+sed -i '' -e "12a\\
 \ \ \ \ fi" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
+sed -i '' -e "13a\\ 
+\ \ \ \ id=\`cat /var/run/JDownloader/JDownloader.pid\`" ${JDOWNLOADER_HOME}/sbin/jdownloader   
+
+sed -i '' -e "14a\\
+\ \ \ \ echo \${id}" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+sed -i '' -e "15a\\
+\ \ \ \ if ps -p \${id} > /dev/null" ${JDOWNLOADER_HOME}/sbin/jdownloader                     
+
+sed -i '' -e "16a\\
+\ \ \ \ \ \ \ \ then" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+sed -i '' -e "17a\\
+\ \ \ \ \ \ \ \ echo \"Another copy of JDownloader appears to be running already."\" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
 sed -i '' -e "18a\\
+\ \ \ \ \ \ \ \ exit" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+sed -i '' -e "19a\\
+\ \ \ \ else" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+sed -i '' -e "20a\\
+\ \ \ \ \ \ \ \ rm /var/run/JDownloader/JDownloader.pid" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+sed -i '' -e "21a\\
+\ \ \ \ fi" ${JDOWNLOADER_HOME}/sbin/jdownloader
+
+sed -i '' -e "22a\\
 fi" ${JDOWNLOADER_HOME}/sbin/jdownloader 
 
-#sed -i '' -e "13a\\
+#sed -i '' -e "23a\\
 #ldconfig -m ${JDOWNLOADER_HOME}/lib" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "32a\\
+sed -i '' -e "24a\\
 if [ -z `pgrep Xvfb` ]; then" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "33a\\
+sed -i '' -e "25a\\
 \ \ \ \ exec /usr/pbi/jdownloader-amd64/bin/Xvfb :1 -screen 0 1024x768x16 &" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "34a\\
+sed -i '' -e "26a\\
 fi" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "35a\\
+sed -i '' -e "27a\\
 if [ -z `pgrep x11vnc` ]; then" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "36a\\
+sed -i '' -e "28a\\
 \ \ \ \ exec /usr/pbi/jdownloader-amd64/bin/x11vnc -noshm -nevershared -forever -display :1 &" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "37a\\
+sed -i '' -e "29a\\
 fi" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "38a\\
+sed -i '' -e "30a\\
 if [ -z `pgrep fluxbox` ]; then" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "39a\\
+sed -i '' -e "31a\\
 \ \ \ \ exec /usr/pbi/jdownloader-amd64/bin/fluxbox -d :1 &" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-sed -i '' -e "40a\\
+sed -i '' -e "32a\\
 fi" ${JDOWNLOADER_HOME}/sbin/jdownloader
 
 # Creat PID in sbin/jdownloader
@@ -137,7 +153,7 @@ fi" ${JDOWNLOADER_HOME}/sbin/jdownloader
 echo "sleep 2" >> ${JDOWNLOADER_HOME}/sbin/jdownloader
 echo "pgrep -U root -f JDownloader.jar > /var/run/JDownloader/JDownloader.pid" >> ${JDOWNLOADER_HOME}/sbin/jdownloader
 
-mkdir -p ${JDOWNLOADER_HOME}/etc/home/jdownloader
+mkdir -p ${JDOWNLOADER_HOME}/etc/home/jdownloader/.fluxbox
 pw groupadd jdown
 pw useradd jdown -g jdown -G wheel -s /bin/sh -d ${JDOWNLOADER_HOME}/etc/home/jdownloader-w none
 chown -R jdown:jdown ${JDOWNLOADER_HOME}/etc/home/jdownloader
