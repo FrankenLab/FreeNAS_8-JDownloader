59f452ec8500aa32b075c64598ef4d9bfcb74579
ProtoSD <sdproto@gmail.com>
2012-09-11 01:28:51 -0700

    Forgot to copy template scripts to their proper destination name

--- nanobsd/plugins/jdownloader_pbi/scripts/post-install.sh
+++ nanobsd/plugins/jdownloader_pbi/scripts/post-install.sh
@@ -25,30 +25,33 @@ rm -rf ${JDOWNLOADER_HOME}/fonts
 #################################
 
 # Changed script user from www to jdown
-sed -i '' -e "s,www,jdown,g" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
+#sed -i '' -e "s,www,jdown,g" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
 
-sed -i '' -e "s,-U jdown,-U root,g" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
+#sed -i '' -e "s,-U jdown,-U root,g" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
 
-sed -i '' -e "25a\\
-_dirs=\"/var/run/JDownloader /var/log/JDownloader\"" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
+#sed -i '' -e "25a\\
+#_dirs=\"/var/run/JDownloader /var/log/JDownloader\"" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
 
 
-sed -i '' -e "26a\\
-start_precmd=\"mkdir -p \$_dirs; touch /var/run/JDownloader/JDownloader.pid; chown -R \$jdownloader_user \$_dirs; ldconfig -m /usr/pbi/jdownloader-amd64/lib\"" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
+#sed -i '' -e "26a\\
+#start_precmd=\"mkdir -p \$_dirs; touch /var/run/JDownloader/JDownloader.pid; chown -R \$jdownloader_user \$_dirs; ldconfig -m /usr/pbi/jdownloader-amd64/lib\"" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
 
-sed -i '' -e "27a\\
-RUN_AS_USER=\"jdown\"" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
+#sed -i '' -e "27a\\
+#RUN_AS_USER=\"jdown\"" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
 
 # Need to put/replace this in /usr/local/etc/rc.d/jdownloaderd
 #command='/usr/pbi/jdownloader-amd64/sbin/jdownloader'
 #sed -i '' -e "s,command=,command=env DISPLAY=:1 ," ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
 
 # Copy template wrapper script over existing script
-cp -a ${JDOWNLOADER_HOME}/sbin_jdownloader ${JDOWNLOADER_HOME}/sbin/
+#cat ${JDOWNLOADER_HOME}/sbin_jdownloader ${JDOWNLOADER_HOME}/sbin/jdownloader
+cp -a ${JDOWNLOADER_HOME}/sbin_jdownloader ${JDOWNLOADER_HOME}/sbin/jdownloader
+chmod 755 ${JDOWNLOADER_HOME}/sbin/jdownloader
 
 # Copy template RC script over existing script
-cp -a ${JDOWNLOADER_HOME}/rc_jdownloaderd ${JDOWNLOADER_HOME}/etc/rc.d/
-cp -a ${JDOWNLOADER_HOME}/rc_jdownloaderd /usr/local/etc/rc.d/
+cp -a ${JDOWNLOADER_HOME}/rc_jdownloaderd ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
+cp -a ${JDOWNLOADER_HOME}/rc_jdownloaderd /usr/local/etc/rc.d/jdownloaderd
+chmod 755 /usr/local/etc/rc.d/jdownloaderd
 
 mkdir -p ${JDOWNLOADER_HOME}/etc/home/jdownloader/.fluxbox
 pw groupadd jdown
