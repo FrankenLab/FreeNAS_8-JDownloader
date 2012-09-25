3023b166581322225f5017ca6da2a541a2632cf6
ProtoSD <sdproto@gmail.com>
2012-09-25 12:59:07 -0700

    Added link for unrar to share/java/JDownloader/tools/linux/unrar

--- nanobsd/plugins/jdownloader_pbi/scripts/post-install.sh
+++ nanobsd/plugins/jdownloader_pbi/scripts/post-install.sh
@@ -16,35 +16,7 @@ rm -rf ${JDOWNLOADER_HOME}/fonts
 #(cd /usr/local/lib/X11/fonts ; tar xf ${JDOWNLOADER_HOME}/fonts.tar)
 #rm ${JDOWNLOADER_HOME}/fonts.tar
 
-##########################
-# SED Stuff
-##########################
-
-#################################
-### rc.d/jdownloaderd changes ###
-#################################
-
-# Changed script user from www to jdown
-#sed -i '' -e "s,www,jdown,g" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
-
-#sed -i '' -e "s,-U jdown,-U root,g" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
-
-#sed -i '' -e "25a\\
-#_dirs=\"/var/run/JDownloader /var/log/JDownloader\"" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
-
-
-#sed -i '' -e "26a\\
-#start_precmd=\"mkdir -p \$_dirs; touch /var/run/JDownloader/JDownloader.pid; chown -R \$jdownloader_user \$_dirs; ldconfig -m /usr/pbi/jdownloader-amd64/lib\"" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
-
-#sed -i '' -e "27a\\
-#RUN_AS_USER=\"jdown\"" ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
-
-# Need to put/replace this in /usr/local/etc/rc.d/jdownloaderd
-#command='/usr/pbi/jdownloader-amd64/sbin/jdownloader'
-#sed -i '' -e "s,command=,command=env DISPLAY=:1 ," ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd
-
 # Copy template wrapper script over existing script
-#cat ${JDOWNLOADER_HOME}/sbin_jdownloader ${JDOWNLOADER_HOME}/sbin/jdownloader
 cp -a ${JDOWNLOADER_HOME}/sbin_jdownloader ${JDOWNLOADER_HOME}/sbin/jdownloader
 chmod 755 ${JDOWNLOADER_HOME}/sbin/jdownloader
 
@@ -71,16 +43,13 @@ chown -R jdown:jdown /var/run/JDownloader /var/log/JDownloader
 ##########################
 
 ln -sf ${JDOWNLOADER_HOME}/bin/unrar /usr/local/bin/unrar
+mkdir -p ${JDOWNLOADER_HOME}/share/java/JDownloader/tools/linux
+ln -sf ${JDOWNLOADER_HOME}/bin/unrar ${JDOWNLOADER_HOME}/share/java/JDownloader/tools/linux/unrar
 ln -sf ${JDOWNLOADER_HOME}/etc/rc.d/jdownloaderd /usr/local/etc/rc.d/jdownloaderd
 
 ldconfig -m ${JDOWNLOADER_HOME}/lib
 ldconfig -m /usr/pbi/jdownloader-`uname -m`/openjdk6/jre/lib/`uname -m`
 
-#ln -sf /usr/pbi/${JDOWNLOADER_HOME}/openjdk6/jre/lib/amd64/xawt/libmawt.so /usr/local/lib/
-#find /usr/pbi/${JDOWNLOADER_HOME}/lib -name "libXrender.*" -exec ln -sf {} /usr/local/lib/ \;
-#find /usr/pbi/${JDOWNLOADER_HOME}/lib -name "libmawt.*" -exec ln -sf {} /usr/local/lib/ \;
-#find /usr/pbi/${JDOWNLOADER_HOME}/lib -name "libXtst.*" -exec ln -sf {} /usr/local/lib/ \;
-#find /usr/pbi/${JDOWNLOADER_HOME}/lib -name "libXi.*" -exec ln -sf {} /usr/local/lib/ \;
 
 ##########################
 # CLEANUP
